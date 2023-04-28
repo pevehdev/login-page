2724981ab3d79d0f79def182af988d6855f824e6
pevehdev <pauloht2020@gmail.com>
2023-04-27 23:58:34 -0300

    indentação

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Routes, Route, Router} from "react-router-dom";
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {App} from './App'
-import {Login} from './components/pages/Login/index.jsx'
 
-import {Register} from './components/pages/Register'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
