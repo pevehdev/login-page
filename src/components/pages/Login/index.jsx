d7a666e64512f68ada4761322e35d29572248766
pevehdev <pauloht2020@gmail.com>
2023-04-28 00:02:11 -0300

    definindo autenticação com email cadastrado

--- src/components/pages/Login/index.jsx
+++ src/components/pages/Login/index.jsx
@@ -1,52 +1,76 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../../assets/react.svg";
 import "./styles.css";
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from "../../../services/firebaseConfig";
+import { useState } from "react";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
+export function Login() {
 
-export function Login(){
-        
-        const [email, setEmail] = useState('');
-        const [password, setPassword] = useState('');
-        const [
-          createUserWithEmailAndPassword,
-          user,
-          loading,
-          error,
-        ] = useCreateUserWithEmailAndPassword(auth);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-        function handleSignIn(e){
-            e.preventDefault();
-            useCreateUserWithEmailAndPassword(email, password)
-        }
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
 
-    return <div className="container">
-        <header className="header">
-            <img src={logo} alt="reactIcone" />
-            <span>Entre com suas credenciais</span>
-        </header>
-        <form >
-            <div className="inputContainer">
-                <label htmlFor="email" > E-nail</label>
-                <input type="text" name="email" id="email" placeholder="johndoe@gmail.com" />
+    function handleSignIn(e) {
+        e.preventDefault();
+      signInWithEmailAndPassword(email, password)
+    }
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error.message}</p>
             </div>
-
-            <div className="inputContainer">
-                <label htmlFor="password" > Password</label>
-                <input type="password" name="password" id="password" placeholder="**********" />
-            </div>
-
-            <a href="#"> Esqueceu sua senha?</a>
-            <button className="button">
-                Entrar <img src="#" alt="" />
-            </button>
-            <div className="footer">
-            <p>Não possui uma conta?</p>
-            <Link to='/register'>Criar uma conta</Link>
-            </div>
-        </form>
-    </div>
-
+        );
+    }
     
+    if (loading) {
+        return <p>Carreganndo...</p>;
+    }
+    
+    if (user) {
+        return (
+            <div>
+                <p>Usuário Logado: {user.user.email}</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="container">
+            <header className="header">
+                <img src={logo} alt="reactIcone" />
+                <span>Entre com suas credenciais</span>
+            </header>
+            <form >
+                <div className="inputContainer">
+                    <label htmlFor="email" > E-nail</label>
+                    <input type="text" name="email" id="email" placeholder="johndoe@gmail.com"
+                        onChange={(e) => setEmail(e.target.value)} />
+                </div>
+
+                <div className="inputContainer">
+                    <label htmlFor="password" > Password</label>
+                    <input type="password" name="password" id="password" placeholder="**********" onChange={(e) => setPassword(e.target.value)} />
+                </div>
+
+                <a href="#"> Esqueceu sua senha?</a>
+                <button className="button" onClick={handleSignIn}>
+                    Entrar <img src="#" alt="" />
+                </button>
+                <div className="footer">
+                    <p>Não possui uma conta?</p>
+                    <Link to='/register'>Criar uma conta</Link>
+                </div>
+            </form>
+        </div>)
+
+
+
+
 }
